c085c35c2c484862f89b7c6401d37298f9f41cc4
Gabriel Takuya Yamamoto <takuyagabriel@gmail.com>
2021-10-30 02:26:05 -0300

    New UI, round count and score functionalities

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 let playerScore = 0
 let computerScore = 0
+let roundScore = 0
 let randomNumber = 0
+let result = ''
+let playerSelection = ''
+
+const rockBtn = document.querySelector('#rock')
+const scissorsBtn = document.querySelector('#scissors')
+const paperBtn = document.querySelector('#paper')
+
+const playerScoreText = document.getElementById('playerScore')
+const computerScoreText = document.getElementById('computerScore')
+const roundScoreText = document.getElementById('roundTitle')
+const resultsContainer = document.getElementById('resultsContainer')
 
 function generateRandomNumber() {
   return (randomNumber = Math.floor(Math.random() * 3))
 }
 
-let result = ''
 function computerPlay() {
-  console.log(randomNumber)
   if (randomNumber === 0) {
     return (result = 'rock')
   } else if (randomNumber === 1) {
@@ -20,56 +30,81 @@ function computerPlay() {
   }
 }
 
-let playerSelection = ''
-const rockBtn = document.querySelector('#rock')
-const scissorsBtn = document.querySelector('#scissors')
-const paperBtn = document.querySelector('#paper')
-rockBtn.addEventListener('click', () => {
-  return (playerSelection = 'rock')
-})
-scissorsBtn.addEventListener('click', () => {
-  return (playerSelection = 'scissors')
-})
-paperBtn.addEventListener('click', () => {
-  return (playerSelection = 'paper')
-})
+function updatePlayerScore(number) {
+  playerScore = number
+  playerScoreText.innerText = `Player Score: ${playerScore}`
+  return playerScore
+}
+
+function updateComputerScore(number) {
+  computerScore = number
+  computerScoreText.innerText = `Computer Score: ${computerScore}`
+  return computerScore
+}
+
+function updateRoundScore(number) {
+  roundScore = number
+  roundScoreText.innerText = `ROUND: ${roundScore}`
+  return roundScore
+}
 
 function playRound() {
   computerSelection = computerPlay()
+  const paragraph = document.createElement('p')
 
   if (playerSelection === computerSelection) {
-    console.log(`Draw!! ${playerSelection} tie with ${computerSelection}`)
+    paragraph.textContent = `Draw!! ${playerSelection} tie with ${computerSelection}`
+    return (
+      resultsContainer.appendChild(paragraph), updateRoundScore(roundScore + 1)
+    )
   } else if (
     // haha yandere dev go brr
     (playerSelection === 'rock' && computerSelection === 'paper') ||
     (playerSelection === 'scissors' && computerSelection === 'rock') ||
-    (playerSelection === 'paper' && computerSelection === 'rock') ||
-    (playerSelection === 'rock' && computerSelection === 'scissors') ||
     (playerSelection === 'paper' && computerSelection === 'scissors')
   ) {
+    paragraph.textContent = `Lose!! ${playerSelection} lost  ${computerSelection} `
     return (
-      console.log(`Lose!! ${playerSelection} lost to ${computerSelection}`),
-      (computerScore = computerScore + 1)
+      updateComputerScore(computerScore + 1),
+      resultsContainer.appendChild(paragraph),
+      updateRoundScore(roundScore + 1)
     )
-  } else if (playerSelection !== 'scissors' && 'rock' && 'paper') {
-    console.log(`${playerSelection} is not a valid type ${computerSelection}`)
-  } else {
+  } else if (
+    (playerSelection === 'rock' && computerSelection === 'scissors') ||
+    (playerSelection === 'scissors' && computerSelection === 'paper') ||
+    (playerSelection === 'paper' && computerSelection === 'rock')
+  ) {
+    paragraph.textContent = `Win!! ${playerSelection} beats ${computerSelection} `
     return (
-      console.log(`Win!! ${playerSelection} beats ${computerSelection}`),
-      (playerScore = playerScore + 1)
+      resultsContainer.appendChild(paragraph),
+      updatePlayerScore(playerScore + 1),
+      updateRoundScore(roundScore + 1)
+    )
+  } else {
+    return console.log(
+      `${playerSelection} is not a valid type ${computerSelection}`
     )
   }
 }
 
+// deactivating all buttons
+// const btnArray = [rockBtn, scissorsBtn, paperBtn]
+// btnArray.forEach((item) => {
+//   item.setAttribute('disabled', true)
+// })
+
 rockBtn.addEventListener('click', () => {
+  playerSelection = 'rock'
   generateRandomNumber()
   playRound()
 })
 scissorsBtn.addEventListener('click', () => {
+  playerSelection = 'scissors'
   generateRandomNumber()
   playRound()
 })
 paperBtn.addEventListener('click', () => {
+  playerSelection = 'paper'
   generateRandomNumber()
   playRound()
 })
